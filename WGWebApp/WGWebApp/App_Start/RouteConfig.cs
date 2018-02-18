a8dc666c0e425b6ae09b2691697985c7d4665c57
Weylin Goodie <goodiew29@lsus.edu>
2018-02-18 08:05:41 -0600

    Custom Route.

--- WGWebApp/WGWebApp/App_Start/RouteConfig.cs
+++ WGWebApp/WGWebApp/App_Start/RouteConfig.cs
@@ -12,7 +12,10 @@ namespace WGWebApp
 		public static void RegisterRoutes(RouteCollection routes)
 		{
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+			routes.MapRoute(
+				name: "Test",
+				url: "{controller}/{action}/{id}",
+				defaults: new { controller = "Test", action = "Action1", id = UrlParameter.Optional });
 			routes.MapRoute(
 				name: "Default",
 				url: "{controller}/{action}/{id}",
